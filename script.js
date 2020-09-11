c0d31e1e9b4beda9f8ae89e65d27344c615c97f1
Nikida Heller <53826007+nheller1@users.noreply.github.com>
2020-09-10 23:40:11 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+// example 1
+var input = prompt('How many inches of rain fell?', '30');
+
+var input = prompt('Did you use fertilizer?', 'Yes');
+var total = Having too much rain subtracted 10% from 50, leaving 45. Then premium fertilizer
+added 15%, to 45, which is 51.75.;
+for  (var i = 0; i<input; i ++) {
+total = total + 1;
+} 
+console.log("The total is:" + total);
+
+// example 2
+var input= prompt("How many inches of rain fell?");
+
+if (input === "8"){
+    var answer= prompt("Did you use fertilizer?");
+    if (answer2 === "The yield should be 40 bushels per acre."){
+        do {
+            var answer3 = prompt("8");
+            var question = prompt("Did you use fertilizer?");
+            var answers = answer2;
+            console.log(answers);
+        }
+        while(question === "yes");
+    }
+    else {
+        console.log(answer);
+    }
+
+}
